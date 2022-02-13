a4f43f9675498d8bf879e872e0fa78cb762e5a41
vic10us <code@dexs.ca>
2022-02-13 11:48:52 -0500

    Migrated to dotnet 6; Added API and openapi spec;

---
+++ bot/Controllers/GuildController.cs
@@ -0,0 +1,47 @@
+﻿using bot.Features.Database;
+using bot.Features.Database.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace bot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuildController : ControllerBase
+    {
+        public readonly BotDataService _botDataService;
+
+        public GuildController(BotDataService botDataService)
+        {
+            _botDataService = botDataService;
+        }
+
+        [HttpGet()]
+        public async Task<IEnumerable<GuildData>> ListAsync()
+        {
+            return await _botDataService.GetGuildsAsync();
+        }
+
+        [HttpGet("{id}")]
+        public GuildData Get(ulong id)
+        {
+            return _botDataService.GetGuild(id);
+        }
+
+        //[HttpPost("{id}")]
+        //public async Task<GuildData> Create(GuildData guildData)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        [HttpPut("{id}")]
+        public void Update([FromRoute] ulong id, [FromBody] GuildData guildData)
+        {
+            if ($"{id}" != guildData.guildId) throw new ArgumentException("Id mismatch", nameof(id));
+            _botDataService.UpdateGuild(id, guildData);
+        }
+    }
+}
---
+++ bot/Features/Database/Models/GuildData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace bot.Features.Database.Models
+{
+    public class GuildData
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string guildId { get; set; }
+        public IDictionary<string, string> channelNotifications { get; set; } = new Dictionary<string, string>();
+
+        public GuildData() {}
+
+        public GuildData(string id, GuildData data)
+        {
+            Id = id;
+            guildId = data.guildId;
+            channelNotifications = data.channelNotifications;
+        }
+
+    }
+}
